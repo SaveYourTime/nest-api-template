afef5e5f0acba75054d1435f10b6ccebe22a2de4
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-08 15:41:04 +0800

    Get all tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): any[] {
+    return this.tasksService.getAllTasks();
+  }
+}
