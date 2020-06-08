e9f6047721f56b00f96079102c31faf5d912032e
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-08 12:58:59 +0800

    Create a tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
