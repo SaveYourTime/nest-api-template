0af0125ebdc83cc377401033516a94a829031119
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-08 17:22:24 +0800

    Get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -10,6 +10,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
