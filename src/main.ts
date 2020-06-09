d5393c6f31a28aabcfb31bba845f41d499544bbf
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-10 01:55:09 +0800

    Setup helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.enableCors();
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
