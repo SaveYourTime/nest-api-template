a7a224d8125f4f009bf147dae6703fa28a7712f3
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-11 01:10:47 +0800

    Remove useless app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
