b9c9d5228f9b19ea3c16aa340ef0455e25f7a914
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-12 17:58:08 +0800

    Setup custom decorator

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from './user.entity';
+
+export const GetUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    return data ? req.user[data] : req.user;
+  },
+);
