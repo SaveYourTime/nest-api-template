9dd6c596750f3a7711d82990e95b748f59ca831a
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-13 11:34:47 +0800

    Setup cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('v1');
   app.enableCors();
   app.use(helmet());
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
