95570bd30399102818f068e6897fba80e5ce99ed
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-14 22:19:37 +0800

    Add rule to eslint - ignore type variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,9 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^type$' },
+    ],
   },
 };
