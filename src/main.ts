08920725264e48a8b6ba204f7cae4694fc015198
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-15 21:27:00 +0800

    Applying logger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.enableCors();
   app.use(helmet());
   app.use(cookieParser());
-  await app.listen(3000);
+  const PORT = 3000;
+  await app.listen(PORT);
+  logger.log(`Nest application listening on port ${PORT}`);
 }
 bootstrap();
