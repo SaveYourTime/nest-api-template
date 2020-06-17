163813bf11c357960ec242b58bd8251458e77d26
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-06-17 10:29:22 +0800

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+
+const setupSwagger = (app: INestApplication): void => {
+  const options = new DocumentBuilder()
+    .setTitle('Tasks Managament')
+    .setDescription('The tasks API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +24,7 @@ async function bootstrap() {
   app.enableCors({ origin: process.env.ORIGIN });
   app.use(helmet());
   app.use(cookieParser());
+  setupSwagger(app);
   await app.listen(process.env.PORT);
 
   const logger = new Logger('bootstrap');
