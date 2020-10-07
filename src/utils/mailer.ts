77201ab9cbe43de4e1c13865a2257e4307eb1b29
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-10-07 16:52:01 +0800

    Add utils functions - mailer and uploadToS3

---
+++ src/utils/mailer.ts
@@ -0,0 +1,37 @@
+import * as SES from 'aws-sdk/clients/ses';
+import { AWSError } from 'aws-sdk/lib/error';
+import { PromiseResult } from 'aws-sdk/lib/request';
+
+interface Mailer {
+  from?: string;
+  to: string[];
+  cc?: string[];
+  subject: string;
+  content: string;
+  reply?: string[];
+}
+
+export const mailer = async ({
+  from = 'service@api.com',
+  to,
+  cc,
+  subject,
+  content,
+  reply,
+}: Mailer): Promise<PromiseResult<SES.SendEmailResponse, AWSError>> => {
+  const params = {
+    Destination: { CcAddresses: cc, ToAddresses: to },
+    Message: {
+      Subject: { Charset: 'UTF-8', Data: subject },
+      Body: { Html: { Charset: 'UTF-8', Data: content } },
+    },
+    Source: from,
+    ReplyToAddresses: reply,
+  };
+
+  const sendPromise = new SES({ apiVersion: '2010-12-01', region: process.env.AWS_REGION })
+    .sendEmail(params)
+    .promise();
+
+  return sendPromise;
+};
---
+++ src/utils/uploadToS3.ts
@@ -0,0 +1,60 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import * as S3 from 'aws-sdk/clients/s3';
+import { extname, join } from 'path';
+import { v4 as uuid } from 'uuid';
+
+const { AWS_BUCKET_NAME = 'bucket-temp' } = process.env;
+
+const s3 = new S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+
+s3.listBuckets((err, data) => {
+  if (err) {
+    throw new InternalServerErrorException(err);
+  } else {
+    const hasBucket = data.Buckets.find((bucket) => bucket.Name === AWS_BUCKET_NAME);
+    if (!hasBucket) {
+      s3.createBucket({ Bucket: AWS_BUCKET_NAME }, (err) => {
+        if (err) {
+          throw new InternalServerErrorException(err);
+        } else {
+          const readOnlyAnonUserPolicy = {
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Sid: 'AddPerm',
+                Effect: 'Allow',
+                Principal: '*',
+                Action: ['s3:GetObject'],
+                Resource: [`arn:aws:s3:::${AWS_BUCKET_NAME}/*`],
+              },
+            ],
+          };
+
+          const bucketPolicyParams = {
+            Bucket: AWS_BUCKET_NAME,
+            Policy: JSON.stringify(readOnlyAnonUserPolicy),
+          };
+
+          s3.putBucketPolicy(bucketPolicyParams, (err) => {
+            if (err) {
+              throw new InternalServerErrorException(err);
+            }
+          });
+        }
+      });
+    }
+  }
+});
+
+export const uploadToS3 = async (
+  file: Express.Multer.File,
+  path = '',
+): Promise<S3.ManagedUpload.SendData> => {
+  const name = `${uuid()}${extname(file.originalname)}`;
+  const Key = join(path, name);
+  const uploadParams = { Bucket: AWS_BUCKET_NAME, Key, Body: file.buffer };
+
+  const uploadPromise = s3.upload(uploadParams).promise();
+
+  return uploadPromise;
+};
